84854bcd3521299ea5a4092d75df5d19e0d13431
byunghyun.fc <byunghyun.fc@gmail>
2024-01-20 17:43:41 +0900

    feat: Aside 찜한 상품 영역 추가하기

--- src/components/layout/JggtLayout/_components/Aside/index.tsx
+++ src/components/layout/JggtLayout/_components/Aside/index.tsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames'
 
+import Likes from './_components/Likes'
 import Recent from './_components/Recent'
 import styles from './style.module.scss'
 
-import Text from '@/components/common/Text'
 import Container from '@/components/layout/Container'
 
 export default function Aside() {
@@ -15,18 +15,7 @@ export default function Aside() {
           'absolute top-8 flex flex-col gap-2 w-24',
         )}
       >
-        <div className="border border-black bg-white p-2 flex flex-col items-center">
-          <Text size="xs">찜한 상품</Text>
-          <Text size="xs" color="grey" className="flex gap-1 items-center mt-1">
-            <span
-              className="material-symbols-outlined"
-              style={{ fontSize: '0.725rem' }}
-            >
-              favorite
-            </span>
-            0
-          </Text>
-        </div>
+        <Likes />
         <Recent />
       </aside>
     </Container>
