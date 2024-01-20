5fd9d8aa91642a4dae8b3ecef10033dcde6faea2
byunghyun.fc <byunghyun.fc@gmail>
2024-01-20 17:43:44 +0900

    feat: 메인 페이지 Migration

--- src/app/_components/ProductList/index.tsx
+++ src/app/_components/ProductList/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+import '@/styles/globals.css'
+import JggtLayout from '@/components/layout/AppJggtLayout'
+
+type Props = {
+  children: ReactNode
+}
+
+export const metadata: Metadata = {
+  title: '중고장터',
+  openGraph: {
+    title: '중고장터',
+  },
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="ko">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <JggtLayout>{children}</JggtLayout>
+      </body>
+    </html>
+  )
+}
