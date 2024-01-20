3dca49f6763052e8230a756b58a23879941752ca
byunghyun.fc <byunghyun.fc@gmail>
2024-01-20 17:43:40 +0900

    feat: 오늘의 상품 추천 컴포넌트에 무한 스크롤 추가하기

--- src/pages/_components/ProductList/index.tsx
+++ src/pages/_components/ProductList/index.tsx
@@ -1,18 +1,55 @@
 import { useState, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 import Product from '@/components/common/Product'
+import Spinner from '@/components/common/Spinner'
 import { getProducts } from '@/repository/products/getProducts'
 import { Product as TProduct } from '@/types'
 
 export default function ProductList() {
   const [products, setProducts] = useState<TProduct[]>([])
+  const { ref, inView } = useInView({ threshold: 1 })
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLastPage, setIsLastPage] = useState<boolean>(false)
+
+  const handleGetProducts = async ({
+    fromPage,
+    toPage,
+  }: {
+    fromPage: number
+    toPage: number
+  }) => {
+    try {
+      setIsLoading(true)
+      const { data } = await getProducts({ fromPage, toPage })
+      setProducts((prevProducts) => [...prevProducts, ...data])
+
+      if (data.length === 0) {
+        setIsLastPage(true)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   useEffect(() => {
-    ;(async () => {
-      const { data } = await getProducts({ fromPage: 0, toPage: 1 })
-      setProducts(data)
-    })()
+    // 컴포넌트가 마운트 되면 products 값을 2페이지 불러온다.
+    handleGetProducts({ fromPage: 0, toPage: 2 })
   }, [])
+
+  // 항상 products 값은 2페이지까지 불러와져 있다고 가정한다.
+  const [page, setPage] = useState<number>(2)
+
+  useEffect(() => {
+    if (inView) {
+      // inView가 true가 되면 새로운 페이지를 불러온다
+      ;(async () => {
+        handleGetProducts({ fromPage: page, toPage: page + 1 })
+        setPage(page + 1)
+      })()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView])
 
   return (
     <div className="my-8">
@@ -28,6 +65,14 @@ export default function ProductList() {
           </div>
         ))}
       </div>
+      {isLoading && (
+        <div className="text-center mt-2">
+          <Spinner />
+        </div>
+      )}
+      {!isLastPage && !!products.length && products.length < 100 && (
+        <div ref={ref} />
+      )}
     </div>
   )
 }
