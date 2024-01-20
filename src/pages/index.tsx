979a1a530c4d6558a8e85f9691f61b10d2886ebc
byunghyun.fc <byunghyun.fc@gmail>
2024-01-20 17:43:40 +0900

    feat: 상품 Mock 함수 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react'
+
 import JggtLayout from '@/components/layout/JggtLayout'
+import { getProduct } from '@/repository/products/getProduct'
+import { Product } from '@/types'
 
 export default function Home() {
+  const [product, setProduct] = useState<Product>()
+  useEffect(() => {
+    getProduct('id').then(({ data }) => setProduct(data))
+  }, [])
   return (
     <JggtLayout>
-      <div className="bg-slate-300" style={{ height: '150vh' }} />
+      <div>
+        <h5> Sample Product </h5>
+      </div>
+      {product && <div> {JSON.stringify(product)}</div>}
     </JggtLayout>
   )
 }
